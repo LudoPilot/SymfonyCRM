bf1ba20deda52c4fbf2207b8eabb705745182056
LudoPilot <e.berardfr@gmail.com>
2023-03-08 19:21:02 +0100

    DashboardController and HomeController created. DoctrineFixturesBundle and ZenstruckFoundryBundle installed.

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,19 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
+// Import the necessary CSS files from the Bootstrap theme
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-grid.css';
+import 'bootstrap/dist/css/bootstrap-reboot.css';
+import '@creative-tim-official/argon-dashboard-free/assets/css/argon-dashboard.css';
+
+// Import the JavaScript files from the Bootstrap theme (optional). Import more later
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+import '@creative-tim-official/argon-dashboard-free/assets/js/argon.js';
+import '@creative-tim-official/argon-dashboard-free/assets/js/plugins/chart.js/dist/chart.min.js';
+import '@creative-tim-official/argon-dashboard-free/assets/js/plugins/chart.js/dist/chart.extension.js';
+
+
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
